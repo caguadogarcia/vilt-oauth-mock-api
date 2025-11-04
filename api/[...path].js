47cadd35aa6fb9e776a883056c30d2e00fced09b
Carlos Aguado Garcia <caguadogarcia@csod.com>
2025-11-04 20:17:07 +0100

    fix: use serverless-http to run Express properly on Vercel

--- api/[...path].js
+++ api/[...path].js
@@ -1,14 +1,6 @@
 // api/[...path].js
 import app from "../server.js";
-import { createServer } from "http";
-import { parse } from "url";
+import serverless from "serverless-http";
 
-// This bridge manually passes req/res into Express
-export default async function handler(req, res) {
-  const parsedUrl = parse(req.url, true);
-  req.url = parsedUrl.path; // keep /api/token etc.
-
-  // Create a Node server instance for Express to use
-  const server = createServer((req_, res_) => app(req_, res_));
-  server.emit("request", req, res);
-}
+const handler = serverless(app);
+export default handler;
